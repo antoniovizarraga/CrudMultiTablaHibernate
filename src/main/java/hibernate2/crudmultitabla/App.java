39680e89c2001d62daad112a88c38245c9590cd3
antoniovizarraga <antonio.vizarraga@iesnervion.es>
2025-02-12 10:44:20 +0100

    Init commit

---
+++ src/main/java/hibernate2/crudmultitabla/App.java
@@ -0,0 +1,10 @@
+package hibernate2.crudmultitabla;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
